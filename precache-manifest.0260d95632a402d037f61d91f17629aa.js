6b18d79c310e556e5fcd74b822672925b7870ade
Dark Hsu <dark@DarkdeMacBook-Pro.local>
2022-04-24 23:36:15 +0800

    modify basedir

---
+++ precache-manifest.0260d95632a402d037f61d91f17629aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f80cb592ab02a1f86b20",
+    "url": "/TravelFunSite/static/css/main.78cae7bb.chunk.css"
+  },
+  {
+    "revision": "f80cb592ab02a1f86b20",
+    "url": "/TravelFunSite/static/js/main.f80cb592.chunk.js"
+  },
+  {
+    "revision": "30b65d3cac2566098325",
+    "url": "/TravelFunSite/static/css/1.8739061b.chunk.css"
+  },
+  {
+    "revision": "30b65d3cac2566098325",
+    "url": "/TravelFunSite/static/js/1.30b65d3c.chunk.js"
+  },
+  {
+    "revision": "c1f71e6df7f7ffcec2bd",
+    "url": "/TravelFunSite/static/js/runtime~main.c1f71e6d.js"
+  },
+  {
+    "revision": "8e49fb835d01c0a27f60c0af46229927",
+    "url": "/TravelFunSite/static/media/default.8e49fb83.jpeg"
+  },
+  {
+    "revision": "4c298533d7d0846d4db5a8bcdb6e70e2",
+    "url": "/TravelFunSite/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c0c274dd32cd9e39f7e7da4a942fcafa.js"
+  "/TravelFunSite/precache-manifest.0260d95632a402d037f61d91f17629aa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/TravelFunSite/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
